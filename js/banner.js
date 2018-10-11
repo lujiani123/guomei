65682356eecc93f04fee63d624093178a009463a
jianiL <1584731241@qq.com>
2018-10-11 21:48:51 +0800

    第一次提交guomei

--- js/banner.js
+++ js/banner.js
@@ -1,11 +1,49 @@
 var oBox=document.getElementById("box");
     var  oLi=document.querySelectorAll("#box li")
     var oSpan=document.querySelectorAll("#span li")
+      var oLeft=document.getElementById("left");
+    var oRight=document.getElementById("right")
+
 
     var  timer=null;
     var  index=0;
 
-    function  zidong() {
+ //点击左边时
+    oRight.onclick=function(){
+        clearInterval(timer);//点击时关闭定时器
+        if(index==oLi.length-1){
+            index=0;
+        }
+        else{
+            index++
+        }
+        for(i=0;i<=oLi.length-1;i++){
+            oLi[i].className="";
+
+        }
+        oLi[index].className="active";
+
+    }
+
+    //点击右边时
+    oLeft.onclick=function(){
+        clearInterval(timer);//点击时关闭定时器
+        if(index==0){
+
+            index=oLi.length-1;
+    }
+    else{
+        index--;
+    }
+    for(i=0;i<=oLi.length-1;i++){
+        oLi[i].className="";
+
+    }
+    oLi[index].className="active"
+    }
+
+
+       function  zidong() {
         timer= setInterval(function() {
             for (var i=0;i<=oLi.length-1;i++) {
 
@@ -17,18 +55,27 @@ var oBox=document.getElementById("box");
                 }
                 for (i = 0; i <= oLi.length - 1; i++) {
                     oLi[i].className = "";
-                    oSpan[i].className = ""
+
                 }
                 oLi[index].className = "active";
-                oSpan[index].className = "active";
-
-
             }
         },3000)
     }
     zidong();
 
+    //鼠标移入盒子时定时器关闭
+    oBox.onmouseenter=function () {
 
+        clearInterval(timer);
+
+    }
+
+    //鼠标移出盒子时自动播放
+    oBox.onmouseleave=function () {
+
+        zidong()
+
+    }
 
     for (var k=0;k<=oSpan.length-1;k++) {
         oSpan[k].a = k;
