d4697113ad95e20bc2184fcdd69e24e7f3e0a140
lujiani123 <1584731231@qq.com>
2018-10-14 01:18:59 +0800

    首页

--- js/banner.js
+++ js/banner.js
@@ -1,3 +1,5 @@
+// 导航下轮播图
+
 var oBox=document.getElementById("box");
     var  oLi=document.querySelectorAll("#box li")
     var oSpan=document.querySelectorAll("#span li")
@@ -95,3 +97,56 @@ var oBox=document.getElementById("box");
 zidong()
 
     }
+
+
+
+//1f
+
+//热卖商品里的轮播图
+
+
+//大的轮播图
+$(function(){
+	
+	$("#itme_list li").hover(function(){
+		var index=$(this).index();
+//		console.log(index);
+		$("#itme_list li").eq(index).addClass("active").siblings("li").removeClass("active")
+		$("#first .main").eq(index).addClass("active").siblings(".main").removeClass("active")
+	})
+})
+
+
+//2f
+$(function(){
+	
+	$("#two_list li").hover(function(){
+		var index=$(this).index();
+//		console.log(index);
+		$("#two_list li").eq(index).addClass("active").siblings("li").removeClass("active")
+		$("#two .main").eq(index).addClass("active").siblings(".main").removeClass("active")
+	})
+})
+
+
+//3f
+$(function(){
+	
+	$("#three_list li").hover(function(){
+		var index=$(this).index();
+//		console.log(index);
+		$("#three_list li").eq(index).addClass("active").siblings("li").removeClass("active")
+		$("#three .main").eq(index).addClass("active").siblings(".main").removeClass("active")
+	})
+})
+
+//4f
+$(function(){
+	
+	$("#four_list li").hover(function(){
+		var index=$(this).index();
+//		console.log(index);
+		$("#four_list li").eq(index).addClass("active").siblings("li").removeClass("active")
+		$("#four .main").eq(index).addClass("active").siblings(".main").removeClass("active")
+	})
+})
